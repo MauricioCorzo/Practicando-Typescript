d4860992744a2524162a476a168bbc7b31243a85
MauricioCorzo <mauricio.corzo@yahoo.com>
2022-10-04 15:26:05 -0300

    Agregado de clases

---
+++ categorias.ts
@@ -0,0 +1,30 @@
+const categorias = [
+    {
+        nombre: "Casa",
+        color: "blanco",
+        numero: "1"
+    },
+    {
+        nombre: "Departamento",
+        color: "blanco",
+        numero: "1"
+    },
+    {
+        nombre: "Almacen",
+        color: "blanco",
+        numero: "2"
+    },
+    {
+        nombre: "Terreno",
+        color: "negro",
+        numero: "2"
+    },
+    {
+        nombre: "Cabaña",
+        color: "negro",
+        numero: "2"
+    }
+]
+
+
+export default categorias
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // npm init -y
 // npm i typescript ts-node
-// tsc --init crea el tsconfig para hacer mas o menos fuerte typescript
+// npx tsc --init crea el tsconfig para hacer mas o menos fuerte typescript
 // cambiar en package.json el script para correr nodemon
-
+import categorias from "./categorias"
 let numero : number = 29;
 
 enum Fases {
@@ -37,6 +37,9 @@ const funcionCualquiera = (arg1: string , arg2: boolean) => {  // si no le pongo
 funcionCualquiera("Mauricio" , true) // OK
 // funcionCualquiera(true, "Mauricio") // Error
 
+interface Hobby2 {
+    nombre: string
+}
 
 // Arrays y Tuplas
 // Array
@@ -44,6 +47,7 @@ let arrayNumbers: number[] = [10,20,30]
 let arrayNumbers2: Array<number> = [10,20,30]
 let arrayStrings: string[] = ["Mauricio", "Eliana", "Gino"]
 let arrayDeTodo: any[] = ["mauricio", true, 24, {apellido: "Corzo"}]
+let arrayDeHobbys2: Hobby2[] = [{nombre: "Leer"} , {nombre: "Jugar Videojuegos"}]
 
 // Array + any
 let arrayDinamico: any[] = [false, "Mauricio" , [1,2,3]]
@@ -57,7 +61,7 @@ let person: [string,number] = ["Mauricio" , 27]
 
 // Objetos
 interface Persona {
-    nombre: string,
+    readonly nombre: string, //Sirve solo para lectura. No se puede modificar, FUnciona para type y class tmb
     apellido: string,
     edad: number,
     hobbies: Hobby[], // array de objetos de la interfase hobby
@@ -76,9 +80,10 @@ const mauricio: Persona = {
     opcional: "Esto es opcional"
 }
 
+
 interface Estudiante extends Persona {
     estaActivo: boolean,
-    saluda: () => void
+    saluda(): string
 }
 
 const eliana : Estudiante = {
@@ -86,9 +91,22 @@ const eliana : Estudiante = {
     apellido: "Almeira",
     edad: 31,
     estaActivo: true,
-    saluda: () => { console.log("Hola") },
+    saluda: (): string => { return `Hola ${eliana.nombre}` },
     hobbies: [{nombre: "salir a comer"}]
 }
+
+const gabi = {
+    nombre: "Gabi",
+    apellido: "Almeira",
+    edad: 36,
+    estaActivo: false,
+    hobbies: [{nombre: "Trabajar"}],
+    comePoco: true
+}
+const gabriela: Persona = gabi
+
+// console.log(gabriela.comePoco) // solo lee las propiedades que coinciden con la interface Persona
+// eliana.saluda()
 
 // // Error, falta propiedad edad
 // const mauricio2: Persona = {
@@ -105,8 +123,8 @@ const eliana : Estudiante = {
 // Clases
 class Humano {
     nombre: string // por defecto, son publicas
-    private edad: number // Solo lo puede leer el padre desde adentro con una funcion como en este ejemplo
-    protected email: string
+    private edad: number // Solo lo puede leer el padre desde adentro con una funcion como en este ejemplo. En JS se usa # y se lo tengo que poner al this.#edad
+    protected email: string // En JS se usa _ y se lo tengo que poner al this._email
     constructor(nombre:string, edad:number, email: string){
         this.nombre = nombre,
         this.edad = edad
@@ -124,16 +142,43 @@ class Hombre extends Humano {
         super(nombre,edad,email)
         this.isActive = isActive
     }
-    funcionPrueba(){
+    getEmail(){
         return `El email es ${this.email}` // Se puede hacer porque protected solo lo puden leer la clase padre y sus hijos
     }
 }
 
 let gino = new Hombre("Gino" , 7 , "gino@gino.com", true)
 
-console.log(gino.funcionPrueba())
+console.log(gino.getEmail())
 // console.log(gino.edad) // No se puede
-console.log(gino.getEdad()) // SI se puede
+console.log(gino.getEdad()) // SI se puede, si tuviera private solo se puede acceder desde la class Humano
+
+
+// Esta clase tiene que tener las propiedades de la interface Persona. Si quiero tener mas de una se le agrega
+// poniendole una "," (coma)
+class MezclarInterfaces implements Persona, Estudiante {
+    nombre: string
+    apellido: string
+    edad: number
+    hobbies: Hobby[]
+    estaActivo: boolean;
+
+    constructor(nombre: string, apellido: string, edad: number, hobbies: Hobby[], estaActivo: boolean){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.hobbies = hobbies
+        this.estaActivo = estaActivo
+    }
+
+    saluda(): string {
+        return `Hola ${this.nombre} ${this.apellido}`
+    }
+}
+
+const mezclaDeInterfaces = new MezclarInterfaces("Mauricio", "Corzo", 27, [{nombre: "Leer LIbros"}], true)
+
+console.log(mezclaDeInterfaces)
 
 
 // Funciones
@@ -175,11 +220,12 @@ function suma2 ( a:number | string, b:number | string): number {
  function suma5(a: number, b:string): string;
  function suma5(a: string, b:string): string;
  function suma5(a: string, b:number): string;
- function suma5(a: number, b:number): number {
-    return a + b
+ function suma5(a: number, b:number): number;
+ function suma5(a:any , b:any) {
+    return a + b;  
  }
 
- let resultado = suma5(2 , "2")
+ let resultado = suma5("2" , "2")
 
 // --Generic Functions
 function firstElement(arr: any[]){
@@ -194,9 +240,59 @@ function firstElement2<Type>(arr: Type[]): Type {
 let array2 = [ 1 , 2 , 3 , 4 ]
 let elemento2 = firstElement2(array2) // La variable elemento es NUMBER, porque lee lo que tiene adentro del arreglo esto es gracias al "Type"
 
-function unirObjetos <U extends object , V extends object> (obj1: U, obj2: V){
+function unirObjetos <U extends object , V extends object> (obj1: U, obj2: V): U & V{
     return { ...obj1, ...obj2 }
 }
+// const prueba = unirObjetos({name:"Mauricio"}, {age: 27})
+// type test1 = Expect<Equal<typeof prueba, { name: string } & { age: number }>>
 
 unirObjetos({nombre: "Mauricio"}, {edad: 27}) //Esto esta ok pq le decimos que si o si recibimos un objeto
 // unirObjetos({nombre: "Mauricio"} , 27) // Esto tira Error
+
+
+interface prueba {
+    nombre: string
+    edad: number
+    duracion: number
+    activo: boolean
+}
+
+
+const loco : any = {
+    nombre: "Mauricio",
+    edad: 22,
+    duracion: 3,
+    activo: "false"
+}
+
+const comprobacion = (objeto: any ): boolean | prueba => {
+    if(!(typeof objeto.nombre == "string")) return false
+    if(!(typeof objeto.edad == "number")) return false
+    if(!(typeof objeto.duracion == "number")) return false
+    if(!(typeof objeto.activo == "boolean")) return false
+    return objeto
+}
+
+console.log(comprobacion(loco))
+
+type prueba2 = { [key:string]: number }
+
+const obj: prueba2 = {
+    uno: 1,
+    dos: 2
+}
+
+type User = {name:object, age:number, role: "admin" | "free"}
+
+type keys = keyof User // Solo acepta "name" , "age" o "role"
+
+const hollaa:keys = "age"
+
+type ValueOf<Obj> = Obj[keyof Obj] // se puede reutilizar donde sea
+
+type valores = ValueOf<User>
+
+// type valores = User[keyof User] // Se puese hacer tmb type ValueOf<Obj> = Obj[keyof Obj] .... type valores = ValueOf<User> 
+
+const holaa2 : valores = "free" // Este si acepta los values de las keys de user
+
